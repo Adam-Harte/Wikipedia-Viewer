d826a5e7dfc97734083af0bd15d231a952a80303
Adam-Harte <adamharte11@hotmail.co.uk>
2017-02-03 23:57:35 +0000

    -added css styling -added google fonts -added font awesome fonts -cleaned up bootstrap styling

--- js/index.js
+++ js/index.js
@@ -5,31 +5,18 @@ $(".search").on('click', function() {
 		console.log(data);
 		$("ul").empty();
 		//go through each elemt of the 2nd array
-		$.each(data[1], function(index, value) {
-			//consoloe.log to test that elemtns print out in desired order
-			console.log(data[1][index]);
-			console.log(data[2][index]);
-			console.log(data[3][index]);
+		$.each(data[1], function(index, value) {		
 			//need a clear method before appenidn so the page stays clean
-			$("#search-results").append("<li><a target=\"_blank\" href=\"" + data[3][index] + "\"><h2>" + data[1][index] + "</h2><p>" + data[2][index] + "</p></a></li>")
+			$("#search-results").append("<li class='list-group-item grey-background'><a style='text-decoration:none;'target=\"_blank\" href=\"" + data[3][index] + "\"><h2>" + data[1][index] + "</h2><p>" + data[2][index] + "</p></a></li>")
 		});
 
 	});
 });
 
 $(".random-search").on('click', function() {
-	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json&callback=?", function(data) {
-		//get the random pages id
-		var id = data.query.random[0].id;
-		console.log(data);
-		console.log(id);
-		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=" + id + "&inprop=url&format=json&callback=?", function(url) {
-			console.log(url.query.pages[id].fullurl);
-			//set the pages object full url property to be used as a link
-			var link = url.query.pages[id].fullurl;
-			//open a new window with the random article
-			window.open(link);
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&origin=*&generator=random&grnnamespace=0&grnlimit=1&prop=info&inprop=url&format=json&callback=?", function(data) {
+		$.each(data.query.pages, function(index, value){
+			window.open(data.query.pages[index].fullurl);
 		});
-
 	});
 });
